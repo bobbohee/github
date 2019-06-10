84feba80ba8780d24f4c5f1ea5576dd63a9e8397
park-bohee <parkbohee0705@gmail.com>
2019-06-10 09:12:28 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/sdh/github_explore/activity/IntroActivity.java
@@ -0,0 +1,34 @@
+package kr.hs.sdh.github_explore.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.hs.sdh.github_explore.R;
+import kr.hs.sdh.github_explore.thread.IntroThread;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Runnable runnable = new IntroThread(handler);
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+}
---
+++ app/src/main/java/kr/hs/sdh/github_explore/thread/IntroThread.java
@@ -0,0 +1,30 @@
+package kr.hs.sdh.github_explore.thread;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class IntroThread implements Runnable {
+
+    private Handler handler;
+
+    public IntroThread() {
+
+    }
+
+    public IntroThread(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void run() {
+        Message msg = new Message();
+        try {
+            Thread.sleep(3000);
+            msg.what = 1;
+            handler.sendEmptyMessage(msg.what);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
